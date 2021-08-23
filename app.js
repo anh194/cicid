4f73e2c2ef498abb926bef21d345a76da3aae3de
anh194 <tminhanh194@gmail.com>
2021-08-23 13:12:10 +0700

    add test

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// Express App Setup
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// Initialization
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+// Express route handlers
+app.get('/test', (req, res) => {
+  res.status(200).send({ text: 'Simple Node App Working!' });
+});
+
+module.exports = app;
